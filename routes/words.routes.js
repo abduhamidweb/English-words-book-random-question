560330b64a4c6217b0c92a0c4b7288b27a12f907
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-04-28 17:44:43 +0500

    👌 IMPROVE:

--- routes/words.routes.js
+++ routes/words.routes.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import WordController from '../controllers/words.contr.js';
-
 const router = express.Router();
 // Barcha so'zlar ro'yxati
 router.get('/words', WordController.getAllWords);
-
 // Yangi so'z qo'shish
 router.post('/words', WordController.createWord);
-
 // Ma'lum bir so'zni ko'rish
 router.get('/words/:id', WordController.getWord);
-
 // Ma'lum bir so'zni yangilash
 router.patch('/words/:id', WordController.updateWord);
 
