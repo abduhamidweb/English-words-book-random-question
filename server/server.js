db35e391128fcf996da056231c2fdca5aed2905d
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-04-28 17:48:34 +0500

    Api uchun ruxsat berildi.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "../utils/mongo.js";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors"
 import booksRoutes from '../routes/books.routes.js';
 import titlesRoutes from "../routes/titles.routes.js"
 import wordsRoutes from "../routes/words.routes.js"
@@ -9,6 +10,7 @@ import usersRoutes from "../routes/users.routes.js"
 import commentsRoutes from "../routes/comments.routes.js"
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors('*'));
 app.use(express.json());
 app.use('/api', booksRoutes);
 app.use('/api', titlesRoutes);
